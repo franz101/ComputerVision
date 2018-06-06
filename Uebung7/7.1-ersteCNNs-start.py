b4dd0373e93ef283ff6927b633fce268d81d756e
root <root@localhost.localdomain>
2018-06-06 16:56:13 +0200

    Uebung 7

---
+++ Uebung7/7.1-ersteCNNs-start.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 12:42:37 2017
+
+@author: wilms
+"""
+
+from keras.datasets import cifar10
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data() #CIFAR-10 Datensatz laden
+ 
